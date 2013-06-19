23ea0476a7227344efffbc7c7696a02de9f507f2
Masahiro Ide <imasahiro9@gmail.com>
2013-06-19 11:43:19 +0900

    rename build dir

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 navi=./template/navi-bar.html
 footer=./template/footer.html
-
+build=./wosd2013
 # update Program-Committee infomation
 konoha ./data/chair.k
 # index.html
-cat $navi ./template/index.html $footer > html/index.html
+cat $navi ./template/index.html $footer > $build/index.html
 
-cat $navi ./template/cfp.html $footer> html/cfp.html
-cat $navi ./template/important-dates.html $footer > html/important-dates.html
+cat $navi ./template/cfp.html $footer> $build/cfp.html
+cat $navi ./template/important-dates.html $footer > $build/important-dates.html
 cat $navi ./template/program-committee-begin.html ./data/chair.html \
-    ./template/program-committee-end.html $footer > html/program-committee.html
-cat $navi ./template/submission.html $footer > html/submission.html
+    ./template/program-committee-end.html $footer > $build/program-committee.html
+cat $navi ./template/submission.html $footer > $build/submission.html
