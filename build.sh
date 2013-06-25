3bae3d6f1a4929fec508a190144d007842961f94
Masahiro Ide <imasahiro9@gmail.com>
2013-06-25 13:13:42 +0900

    update invited takl

--- build.sh
+++ build.sh
@@ -13,3 +13,4 @@ cat $navi ./template/important-dates.html $footer > $build/important-dates.html
 cat $navi ./template/program-committee-begin.html ./data/chair.html \
     ./template/program-committee-end.html $footer > $build/program-committee.html
 cat $navi ./template/submission.html $footer > $build/submission.html
+cat $navi ./template/invited-talk.html $footer > $build/invited-talk.html
